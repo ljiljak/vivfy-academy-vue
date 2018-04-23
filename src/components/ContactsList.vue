1519c46ac6066969e868e853009c7193d0f888be
ljiljak <ljiljakaleksandar@gmail.com>
2018-04-23 21:39:48 +0200

    on click form

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    
+    <form @submit.prevent="addContact">
+
+      <label>First Name</label>
+      <input v-model="newContact.firstName" type="text" placeholder="First Name"/>
+
+      <label>Last Name</label>
+      <input v-model="newContact.lastName" type="text" placeholder="Last Name"/>
+
+      <label>Email</label>
+      <input v-model="newContact.email" type="email" placeholder="Email"/>
+
+      <button type="submit">Add Contact</button>
+
+    </form>
+
+
+
     <table>
       <thead>
         <th>First Name</th>
@@ -8,8 +26,7 @@
       </thead>
 
       <tbody>
-        <tr v-for="(contact, key) in contacts" :key="key" 
-        v-if="contact.firstName === 'Jack'">
+        <tr v-for="(contact, key) in contacts" :key="key">
         <td>{{ contact.firstName }}</td>
         <td>{{ contact.lastName }}</td>
         <td>{{ contact.email }}</td>
@@ -33,7 +50,19 @@ export default {
         
         { firstName: 'Aleksandar', lastName: 'Ljiljak', email: 'aleksandarljiljak@gmail.com'}
       
-      ]
+      ],
+
+      newContact: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = {}
     }
   }
 }
